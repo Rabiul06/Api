720365b09f4a3327181923ea45571bfa428b3051
Rabiul isalm <robecse99@gmail.com>
2021-03-25 17:35:11 +0600

    Loging regidtration api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        $credentials = $request->only('name','email', 'password');
 
         if ($token = $this->guard()->attempt($credentials)) {
             return $this->respondWithToken($token);
